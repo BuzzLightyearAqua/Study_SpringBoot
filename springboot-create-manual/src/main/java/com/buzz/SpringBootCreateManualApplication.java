28455398af887a83771141af4516b0090fe2abba
Qingyuan <buzzlightyear0131@163.com>
2024-07-31 20:27:56 +0800

    20240731

---
+++ springboot-create-manual/src/main/java/com/buzz/SpringBootCreateManualApplication.java
@@ -0,0 +1,14 @@
+package com.buzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class SpringBootCreateManualApplication {
+      public static void main(String[] args) {
+            SpringApplication.run(SpringBootCreateManualApplication.class);
+      }
+}
